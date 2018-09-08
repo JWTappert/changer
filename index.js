0024858c9319a7ee76e17d5846519fe14f18ebd0
JWTappert <tappertj@oregonstate.edu>
2018-09-08 10:25:02 -0700

    FAILING

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
-function makeChange(amount, tendered) {
-	let change = tendered - amount;
-	return change;
-}
-module.exports = makeChange;
+const changer = {
+	denoms: [
+		["penny", 1],
+		["nickel", 5],
+		["dime", 10],
+		["quarter", 25],
+		["one", 100],
+		["five", 500],
+		["ten", 1000],
+		["twenty", 2000],
+		["fifty", 5000],
+		["hundred", 10000]
+	],
+
+	coins: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+
+	makeChange(amount, tendered) {
+		let change = this.calculateDifference(amount, tendered);
+		let totalPennies = this.toPennies(change);
+		return this.calculateCoins(totalPennies);
+	},
+
+	calculateCoins(pennies) {
+		if (pennies == 0) return coins;
+		// while (pennies > 0) {
+		for (let i = 9; i >= 0; i--) {
+			if (pennies - this.denoms[i][1] >= 0) {
+				console.log(pennies - this.denoms[i][1]);
+				this.coins[i]++;
+			}
+			// this.calculateCoins(pennies);
+		}
+		// }
+	},
+
+	calculateDifference(amount, tendered) {
+		return tendered - amount;
+	},
+
+	toPennies(amount) {
+		return amount * 100;
+	}
+};
+module.exports = changer;
--- index.test.js
+++ index.test.js
@@ -1,5 +1,18 @@
-const makeChange = require("./index");
+const changer = require("./index");
 
 test("amount is $2.75, tendered is $5, result should be $2.25", () => {
-	expect(makeChange(2.75, 5)).toBe(2.25);
+	expect(changer.makeChange(2.75, 5)).toBe(2.25);
+});
+
+test("correctly convert change amount to pennies for easy calculations", () => {
+	expect(changer.toPennies(2.75)).toBe(275);
+	expect(changer.toPennies(100.72)).toBe(10072);
+});
+
+test("calculate difference between amount tendered and amount due", () => {
+	expect(changer.calculateDifference(2.75, 5)).toBe(2.25);
+});
+
+test("returns correct amount of pennies", () => {
+	expect(changer.makeChange(0.1, 0.25)).toBe([0, 1, 1, 0, 0, 0, 0, 0, 0, 0]);
 });
